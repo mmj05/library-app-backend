d32bf88cdbd45e2ebfd1da619f4f472f668ac154
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-17 10:23:13 -0500

    set up Book repository

--- src/main/java/com/jobayer/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/jobayer/springbootlibrary/dao/BookRepository.java
@@ -1,2 +1,8 @@
-package com.jobayer.springbootlibrary.dao;public class BookRepository {
+package com.jobayer.springbootlibrary.dao;
+
+import com.jobayer.springbootlibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
 }
