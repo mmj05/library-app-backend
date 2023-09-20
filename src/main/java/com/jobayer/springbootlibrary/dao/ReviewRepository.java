f46fa0e1818ebadc962546f81e7aa9f9e373105f
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-20 12:13:26 -0500

    Create ReviewRepository which extends JpaRepository

--- src/main/java/com/jobayer/springbootlibrary/dao/ReviewRepository.java
+++ src/main/java/com/jobayer/springbootlibrary/dao/ReviewRepository.java
@@ -1,2 +1,12 @@
-package com.jobayer.springbootlibrary.dao;public interface ReviewRepository {
+package com.jobayer.springbootlibrary.dao;
+
+import com.jobayer.springbootlibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@RequestParam("book_id" ) Long bookId, Pageable pageable);
 }
