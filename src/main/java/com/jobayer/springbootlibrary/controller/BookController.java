db58f3989b5b790dd20228731b138e6a89706666
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-21 23:27:03 -0500

    Create a BookService service class and BookController controller class to add custom api endpoint

--- src/main/java/com/jobayer/springbootlibrary/controller/BookController.java
+++ src/main/java/com/jobayer/springbootlibrary/controller/BookController.java
@@ -1,2 +1,25 @@
-package com.jobayer.springbootlibrary.controller;public class BookController {
+package com.jobayer.springbootlibrary.controller;
+
+import com.jobayer.springbootlibrary.entity.Book;
+import com.jobayer.springbootlibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
 }
