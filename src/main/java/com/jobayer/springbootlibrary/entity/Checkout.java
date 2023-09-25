2171e9618bdc904172794b5c26b68dbfe7735df6
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-25 11:14:44 -0500

    Fix import statements

--- src/main/java/com/jobayer/springbootlibrary/entity/Checkout.java
+++ src/main/java/com/jobayer/springbootlibrary/entity/Checkout.java
@@ -1,6 +1,6 @@
 package com.jobayer.springbootlibrary.entity;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Data;
 
 @Entity
---
+++ src/main/java/com/jobayer/springbootlibrary/requestmodels/ReviewRequests.java
@@ -0,0 +1,2 @@
+package com.jobayer.springbootlibrary.requestmodels;public class ReviewRequests {
+}
