170fb592f050f60a222b00978236fbbe8c10f7ce
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-24 23:06:11 -0500

    Add functionality to utilize ExtractJWT class's payloadJWTExtraction method to extract email from bearer token

--- src/main/java/com/jobayer/springbootlibrary/controller/BookController.java
+++ src/main/java/com/jobayer/springbootlibrary/controller/BookController.java
@@ -2,6 +2,7 @@ package com.jobayer.springbootlibrary.controller;
 
 import com.jobayer.springbootlibrary.entity.Book;
 import com.jobayer.springbootlibrary.service.BookService;
+import com.jobayer.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,20 +19,20 @@ public class BookController {
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount() {
-        String userEmail = "testuser@email.com";
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId) {
-        String userEmail = "testuser@email.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
-        String userEmail = "testuser@email.com";
+    public Book checkoutBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 
