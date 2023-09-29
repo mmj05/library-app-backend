7f90548c09d0ec8a8f0b3e4222289a87c134e644
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-28 19:45:46 -0500

    Create MessageService

--- src/main/java/com/jobayer/springbootlibrary/service/MessagesService.java
+++ src/main/java/com/jobayer/springbootlibrary/service/MessagesService.java
@@ -1,2 +1,25 @@
-package com.jobayer.springbootlibrary.service;public class MessagesService {
+package com.jobayer.springbootlibrary.service;
+
+import com.jobayer.springbootlibrary.dao.MessageRepository;
+import com.jobayer.springbootlibrary.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MessagesService {
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessagesService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public void postMessage(Message messageRequest, String userEmail) {
+        Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
+        message.setUserEmail(userEmail);
+        messageRepository.save(message);
+    }
 }
