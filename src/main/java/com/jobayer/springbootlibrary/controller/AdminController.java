1121c4e2f66ed506962bdced58adad696d85e1a6
Muhaimin Jobayer <mmj0993@gmail.com>
2023-10-01 14:37:30 -0500

    Create AdminController class

---
+++ src/main/java/com/jobayer/springbootlibrary/controller/AdminController.java
@@ -0,0 +1,30 @@
+package com.jobayer.springbootlibrary.controller;
+
+import com.jobayer.springbootlibrary.requestmodels.AddBookRequest;
+import com.jobayer.springbootlibrary.service.AdminService;
+import com.jobayer.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("api/admin")
+public class AdminController {
+
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value = "Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.postBook(addBookRequest);
+    }
+}
