83f1139ab19130dbedb2adc2862c5b0d53a9cd74
Muhaimin Jobayer <mmj0993@gmail.com>
2023-10-02 22:51:19 -0500

    Create PaymentInfoRequest model

---
+++ src/main/java/com/jobayer/springbootlibrary/requestmodels/PaymentInfoRequest.java
@@ -0,0 +1,12 @@
+package com.jobayer.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfoRequest {
+
+    private int amount;
+    private String currency;
+    private String receiptEmail;
+
+}
