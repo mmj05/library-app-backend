396827a1358421c5700ff4f0bbfab6ad4b7ab352
muhaiminjobayer <mmj0993@gmail.com>
2025-07-05 11:33:49 -0500

    Update BookRepository to use case-insensitive search for titles by renaming method to findByTitleContainingIgnoreCase.

--- src/main/java/com/jobayer/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/jobayer/springbootlibrary/dao/BookRepository.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+    Page<Book> findByTitleContainingIgnoreCase(@RequestParam("title") String title, Pageable pageable);
 
     Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 
